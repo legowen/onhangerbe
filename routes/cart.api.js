9583841920c86245734dab730db62e893d1d163b
OwenSyKim <owen.sykim@gmail.com>
2023-12-19 16:58:38 -0800

    Add CartController. addItem, getCart

--- routes/cart.api.js
+++ routes/cart.api.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
+const cartController = require("../controllers/cart.controller");
 
 router.post("/", authController.authenticate, cartController.addItemToCart);
+router.get("/", authController.authenticate, cartController.getCart);
 
 module.exports = router;
