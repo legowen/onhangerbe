b2559e38fc161ed3d154c3c4aa616053c5119ddf
OwenSyKim <owen.sykim@gmail.com>
2023-12-29 15:07:13 -0800

    Add Clear Cart

--- routes/cart.api.js
+++ routes/cart.api.js
@@ -5,12 +5,12 @@ const cartController = require("../controllers/cart.controller");
 
 router.post("/", authController.authenticate, cartController.addItemToCart);
 router.get("/", authController.authenticate, cartController.getCart);
+router.put("/:id", authController.authenticate, cartController.editCartItem);
 router.delete(
   "/:id",
   authController.authenticate,
   cartController.deleteCartItem
 );
-router.put("/:id", authController.authenticate, cartController.editCartItem);
 router.get("/qty", authController.authenticate, cartController.getCartQty);
 
 
