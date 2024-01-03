7bcb36561e1fe93d04a3ebb7b304fd894fd2d21a
OwenSyKim <owen.sykim@gmail.com>
2024-01-02 23:20:40 -0800

    Add OrderAdminPage

--- routes/order.api.js
+++ routes/order.api.js
@@ -5,5 +5,12 @@ const router = express.Router();
 
 router.post("/", authController.authenticate, orderController.createOrder);
 router.get("/me", authController.authenticate, orderController.getOrder);
+router.get("/", authController.authenticate, orderController.getOrderList);
+router.put(
+  "/:id",
+  authController.authenticate,
+  authController.checkAdminPermission,
+  orderController.updateOrder
+);
 
 module.exports = router;
