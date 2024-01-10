1d9fed28e18846dc47079f94ef62fd54d7e1839f
OwenSyKim <owen.sykim@gmail.com>
2024-01-10 15:26:11 -0800

    Add loginWithGoogle

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,10 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const { OAuth2Client } = require("google-auth-library");
 require("dotenv").config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 
 const authController = {};
 
@@ -15,14 +17,46 @@ authController.loginWithEmail = async (req, res) => {
       if (isMatch) {
         const token = await user.generateToken();
         return res.status(200).json({ status: "success", user, token });
-        // token
+        // token & return
       }
     }
     throw new Error("Invalid email or password");
   } catch (error) {
-    res.status(400).json({ status: "fail", error: error.message });
+    return res.status(400).json({ status: "fail", error: error.message });
   }
 };
+
+authController.loginWithGoogle = async (req, res) => {
+  try {
+    const { token } = req.body;
+    const googleClient = new OAuth2Client(GOOGLE_CLIENT_ID);
+    const ticket = await googleClient.verifyIdToken({
+      idToken: token,
+      audiemce: GOOGLE_CLIENT_ID,
+    });
+    const { email, name } = ticket.getPayload();
+    // console.log("eee", email, name);
+    let user = await User.findOne({email})
+    if(!user){
+      //Create new user
+      const randomPassword = ""+Math.floor(Math.random()*100000000)
+      const salt = await bcrypt.genSalt(10)
+      const newPassword = await bcrypt.hash(randomPassword, salt)
+      user = new User({
+        name, 
+        email,
+        password: newPassword
+      })
+      await user.save()
+    }
+    // Create Token & return
+    const sessionToken = await user.generateToken()
+    res.status(200).json({status:"success", user, token:sessionToken})
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
 authController.authenticate = async (req, res, next) => {
   try {
     const tokenString = req.headers.authorization;
@@ -45,7 +79,7 @@ authController.checkAdminPermission = async (req, res, next) => {
     if (user.level !== "admin") throw new Error("You have no permission");
     next();
   } catch (error) {
-    res.status(400).json({ status: "fail", error: error.message });
+    return res.status(400).json({ status: "fail", error: error.message });
   }
 };
 
